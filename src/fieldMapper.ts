44b381e2ef9b33f243818bdd4b072df007c58688
Petter Kjelkenes <petter.kjelkenes@aplia.no>
2023-08-31 15:45:02 +0200

    feat: update fieldMapper to get the whole imageShop data object, rename package to imageshop-org

--- src/fieldMapper.ts
+++
@@ -1,8 +0,0 @@
-const fieldMapper = (asset: any, documentProps: any): any => {
-  // Do custom mapping of fields here. Example:
-  // asset.altText = { nb: documentProps.description }
-
-  return asset
-}
-
-export default fieldMapper
